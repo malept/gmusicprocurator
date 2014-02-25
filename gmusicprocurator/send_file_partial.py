bf4de79a081d93fb3c8d385c29e77f3cc0d4abd4
Mark Lee <gmusicprocurator@lazymalevolence.com>
2014-02-25 15:57:26 -0800

    send_file_partial: changes from the blog post comments
    Off-by-one change plus path checks

--- gmusicprocurator/send_file_partial.py
+++ gmusicprocurator/send_file_partial.py
@@ -6,8 +6,23 @@
 #
 # Original URL:
 #http://blog.asgaard.co.uk/2012/08/03/http-206-partial-content-for-flask-python
+#
+# Modifications Copyright (C) 2014  Mark Lee
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from flask import request, send_file, Response
+from flask import g, request, send_file, Response
 import mimetypes
 import os
 import re
@@ -36,18 +51,21 @@ def send_file_partial(path):
     byte1, byte2 = 0, None
 
     m = re.search('(\d+)-(\d*)', range_header)
-    g = m.groups()
+    groups = m.groups()
 
-    if g[0]:
-        byte1 = int(g[0])
-    if g[1]:
-        byte2 = int(g[1])
+    if groups[0]:
+        byte1 = int(groups[0])
+    if groups[1]:
+        byte2 = int(groups[1])
 
     length = size - byte1
     if byte2 is not None:
-        length = byte2 - byte1
+        length = byte2 - byte1 + 1
 
     data = None
+
+    if not os.path.isabs(path):
+        path = os.path.join(g.current_app.root_path, path)
     with open(path, 'rb') as f:
         f.seek(byte1)
         data = f.read(length)
