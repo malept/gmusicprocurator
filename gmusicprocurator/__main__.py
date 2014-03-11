eec281dc0bd7eed9fdc34eaa8951de56ecc949a1
Mark Lee <gmusicprocurator@lazymalevolence.com>
2014-03-11 13:48:21 -0700

    Add CLI subcommand to list GMusic device IDs

--- gmusicprocurator/__main__.py
+++ gmusicprocurator/__main__.py
@@ -16,12 +16,41 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function
+
 from flask.ext.assets import ManageAssets
 from flask.ext.script import Manager
+from functools import partial
+from gmusicapi.clients import Webclient
 from gmusicprocurator.app import app, assets
+import sys
 
 manager = Manager(app)
 manager.add_command('assets', ManageAssets(assets))
+no_bool_option = partial(manager.option, action='store_false', default=True)
+
+
+@no_bool_option('--no-desktop', dest='show_desktop', help='Hide desktop IDs')
+@no_bool_option('--no-mobile', dest='show_mobile', help='Hide mobile IDs')
+def list_devices(show_desktop, show_mobile):
+    '''
+    List device IDs registered with Google Music. Defaults to showing both
+    desktop and mobile IDs.
+    '''
+    webclient = Webclient()
+    success = webclient.login(app.config['GACCOUNT_EMAIL'],
+                              app.config['GACCOUNT_PASSWORD'])
+    if not success:
+        print('Login failed.', file=sys.stderr)
+        return
+
+    for device in webclient.get_registered_devices():
+        dname = device['name'] if len(device['name']) > 0 else device['model']
+        if not show_desktop and device['type'] == 'DESKTOP_APP':
+            continue
+        if not show_mobile and device['type'] == 'PHONE':
+            continue
+        print(u'* {dname} ({type}): {id}'.format(dname=dname, **device))
 
 
 def run():
