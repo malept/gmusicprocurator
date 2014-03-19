d1a2750fc798b5a01a7378c7997ca0b7d7db2d14
Mark Lee <gmusicprocurator@lazymalevolence.com>
2014-03-19 10:47:57 -0700

    Add scss-lint to the test runner

--- run_tests.sh
+++ run_tests.sh
@@ -8,5 +8,6 @@ MAIN_OUT_CSS="${SCSS_DIR}"/main.out.css
 
 flake8 --exclude=static "${GMP_DIR}"
 coffeelint "${STATIC_DIR}"/cs
+scss-lint -e '*.css' "${SCSS_DIR}"
 scss --style expanded "${SCSS_DIR}"/main.scss "${MAIN_OUT_CSS}"
 csslint "${MAIN_OUT_CSS}"
