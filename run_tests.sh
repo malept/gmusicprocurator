8aec260f853846d6e6bb5a3036be0fbce5abcb36
Mark Lee <gmusicprocurator@lazymalevolence.com>
2014-03-23 17:16:35 -0700

    Add coffeelint config file

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,7 @@ SCSS_DIR="${STATIC_DIR}"/scss
 MAIN_OUT_CSS="${SCSS_DIR}"/main.out.css
 
 flake8 --exclude=static "${GMP_DIR}"
-coffeelint "${STATIC_DIR}"/cs
+coffeelint -f "${BASE_DIR}"/.coffeelint.json "${STATIC_DIR}"/cs
 scss-lint -e '*.css' "${SCSS_DIR}"
 scss --style expanded "${SCSS_DIR}"/main.scss "${MAIN_OUT_CSS}"
 csslint "${MAIN_OUT_CSS}"
