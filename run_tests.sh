df28c86c52c998d9b44a5a72fdf95d7872459dbd
Mark Lee <gmusicprocurator@lazymalevolence.com>
2014-03-27 18:35:28 -0700

    run_tests: show checkers' commands when they're executed

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,7 @@ STATIC_DIR="${GMP_DIR}"/static
 SCSS_DIR="${STATIC_DIR}"/scss
 MAIN_OUT_CSS="${SCSS_DIR}"/main.out.css
 
+set -x
 flake8 --exclude=static "${GMP_DIR}"
 coffeelint -f "${BASE_DIR}"/.coffeelint.json "${STATIC_DIR}"/cs
 scss-lint -e '*.css' "${SCSS_DIR}"
