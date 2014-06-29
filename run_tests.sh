784b2699ba6f39b0463009d377367211ea156146
Mark Lee <gmusicprocurator@lazymalevolence.com>
2014-06-29 10:20:59 -0700

    Test runner: create config file if missing

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,8 @@ GMP_DIR="${BASE_DIR}"/gmusicprocurator
 STATIC_DIR="${GMP_DIR}"/static
 SCSS_DIR="${STATIC_DIR}"/scss
 MAIN_OUT_CSS="${SCSS_DIR}"/main.out.css
+CFG_DIR="$HOME/.config/gmusicapi"
+CFG_PATH="$CFG_DIR"/gmusicprocurator.cfg
 
 set -x
 
@@ -19,5 +21,9 @@ if [[ -z "$NO_FRONTEND" ]]; then
     scss-lint -e '*.css' "${SCSS_DIR}"
     scss --style expanded "${SCSS_DIR}"/main.scss "${MAIN_OUT_CSS}"
     csslint "${MAIN_OUT_CSS}"
+    if [[ ! -f "$CFG_PATH" ]]; then
+        mkdir -p "$CFG_DIR"
+        touch "$CFG_PATH"
+    fi
     python -m gmusicprocurator assets build --no-cache
 fi
